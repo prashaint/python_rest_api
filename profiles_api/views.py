eb331c8bc783acab373953851a07be03ce6bfe2d
Prashaint Mishra <prashaint_mishra@hotmail.com>
2020-11-16 23:04:41 +0530

    Profile API is now up

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -96,5 +97,10 @@ class HelloViewSet(viewsets.ViewSet):
 		return Response({'http_method': 'DELETE'})
 
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+	"""Creates and updates a user profile"""
+	serializer_class = serializers.UserProfileSerializer
+	queryset = models.UserProfile.objects.all()
+	
 
   		  		
