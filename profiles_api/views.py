96f312e4aeae818099ef8a94ded846523f31bdd1
Prashaint Mishra <prashaint_mishra@hotmail.com>
2020-11-17 10:08:41 +0530

    Eanbled check on users to update their own profile only

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
 from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -101,6 +103,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	"""Creates and updates a user profile"""
 	serializer_class = serializers.UserProfileSerializer
 	queryset = models.UserProfile.objects.all()
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (permissions.UpdateOwnProfile,)
 	
 
   		  		
